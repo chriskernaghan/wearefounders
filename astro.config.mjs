1842a4783dcd7d5282d1e731797a13910d680d36
Chris Kernaghan <hello@wearefounders.uk>
2026-05-07 22:11:59 +0100

    Remove base path for direct Pages serving

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,7 +6,6 @@ import { defineConfig } from 'astro/config';
 // for testing, the project will live at [project].pages.dev/directory/.
 export default defineConfig({
   site: 'https://wearefounders.uk',
-  base: '/directory',
   trailingSlash: 'always',
   output: 'static',
   build: {
